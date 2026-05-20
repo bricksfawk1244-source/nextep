07c19aeef829106836e1f26f69af2ea8bd15e8bf
bricksfawk1244-source <bricksfawk1244@gmail.com>
2026-05-20 22:50:09 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,18 +1,5 @@
 export default function handler(req, res) {
-  try {
-    if (req.method !== "POST") {
-      return res.status(405).json({ reply: "POST만 가능" });
-    }
-
-    const message = req.body?.message;
-
-    return res.status(200).json({
-      reply: "Nextep 응답: " + message
-    });
-
-  } catch (err) {
-    return res.status(500).json({
-      reply: "서버 에러"
-    });
-  }
+  return res.status(200).json({
+    reply: "test ok"
+  });
 }
